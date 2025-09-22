d9523ae58d6a86df31eafaee68cd3e2f397aecac
Capozi <capozi.devv@gmail.com>
2025-09-22 09:50:17 -0400

    did da claws kinda

--- src/main/java/net/eya/penumbra/common/item/DecadanceClawsItem.java
+++ src/main/java/net/eya/penumbra/common/item/DecadanceClawsItem.java
@@ -3,13 +3,15 @@ package net.eya.penumbra.common.item;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
-public class DecadanceClawsItem extends Item {
-    public DecadanceClawsItem(Settings settings) {
-        super(settings);
+public class DecadanceClawsItem extends SwordItem {
+    public DecadanceClawsItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
     }
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
--- src/main/java/net/eya/penumbra/foundation/ItemInit.java
+++ src/main/java/net/eya/penumbra/foundation/ItemInit.java
@@ -3,6 +3,7 @@ package net.eya.penumbra.foundation;
 import net.eya.penumbra.Penumbra;
 import net.eya.penumbra.common.item.DecadanceClawsItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -12,5 +13,5 @@ public class ItemInit {
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(Penumbra.MOD_ID, name), item);
     }
-    public static final Item DECADENCE_CLAWS = registerItem("claws_of_decadence", new DecadanceClawsItem(new Item.Settings().maxCount(1)));
+    public static final Item DECADENCE_CLAWS = registerItem("claws_of_decadence", new DecadanceClawsItem(ToolMaterials.NETHERITE, 4, -2.6f, new Item.Settings().maxCount(1)));
 }
